e96d4bc0cadb67953b39c389284777e5fa33e1e9
Frank14b <franckfontcha@gmail.com>
2024-06-02 23:33:30 +0400

    update chat users loading end

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  viewport: 'none', // or 'none' to completely disable
   images: {
     remotePatterns: [
       {
