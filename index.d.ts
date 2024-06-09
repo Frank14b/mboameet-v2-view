030e7e1a77c523833933b3b50404a7a3b9721ea6
Frank14b <franckfontcha@gmail.com>
2024-06-09 16:14:09 +0400

    update types global

---
+++ index.d.ts
@@ -0,0 +1,22 @@
+import "react";
+
+declare global {
+	interface Window {
+		opera: any;
+		Worker: Worker;
+	}
+}
+
+declare module "react" {
+  interface HTMLAttributes<T> {
+    onPointerEnterCapture?: (e: React.PointerEvent<T>) => void;
+    onPointerLeaveCapture?: (e: React.PointerEvent<T>) => void;
+  }
+  
+  interface RefAttributes<T> {
+    onPointerEnterCapture?: (e: React.PointerEvent<T>) => void;
+    onPointerLeaveCapture?: (e: React.PointerEvent<T>) => void;
+  }
+}
+
+export {};
--- src/app/types/index.d.ts
+++ src/app/types/index.d.ts
@@ -1,24 +1,3 @@
-import "react";
-
-declare global {
-	interface Window {
-		opera: any;
-		Worker: Worker;
-	}
-}
-
-declare module "react" {
-  interface HTMLAttributes<T> {
-    onPointerEnterCapture?: (e: React.PointerEvent<T>) => void;
-    onPointerLeaveCapture?: (e: React.PointerEvent<T>) => void;
-  }
-  
-  interface RefAttributes<T> {
-    onPointerEnterCapture?: (e: React.PointerEvent<T>) => void;
-    onPointerLeaveCapture?: (e: React.PointerEvent<T>) => void;
-  }
-}
-
 type ObjectTypeDto = { [key: string]: string | number | unknown | Array<string> | Array<number> };
 
 export interface DebounceFunction {
